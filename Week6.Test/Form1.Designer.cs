a1e297e7a07e97b42dc469263f37e98c1e943707
Abdul Aziz <aabdul9700@gmail.com>
2023-01-09 17:50:25 +0530

    Add project files.

---
+++ Week6.Test/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Week6.Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmployeeNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmpSal = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ContractorRadioButton = new System.Windows.Forms.RadioButton();
+            this.PermanentRadioButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee No";
+            // 
+            // txtEmployeeNo
+            // 
+            this.txtEmployeeNo.Location = new System.Drawing.Point(260, 56);
+            this.txtEmployeeNo.Name = "txtEmployeeNo";
+            this.txtEmployeeNo.Size = new System.Drawing.Size(100, 26);
+            this.txtEmployeeNo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Employee Name";
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(262, 115);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(100, 26);
+            this.txtEmployeeName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Salary";
+            // 
+            // txtEmpSal
+            // 
+            this.txtEmpSal.Location = new System.Drawing.Point(262, 170);
+            this.txtEmpSal.Name = "txtEmpSal";
+            this.txtEmpSal.Size = new System.Drawing.Size(100, 26);
+            this.txtEmpSal.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(504, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Employee Type";
+            // 
+            // ContractorRadioButton
+            // 
+            this.ContractorRadioButton.AccessibleName = "Employee Type";
+            this.ContractorRadioButton.AutoSize = true;
+            this.ContractorRadioButton.Location = new System.Drawing.Point(508, 110);
+            this.ContractorRadioButton.Name = "ContractorRadioButton";
+            this.ContractorRadioButton.Size = new System.Drawing.Size(80, 24);
+            this.ContractorRadioButton.TabIndex = 7;
+            this.ContractorRadioButton.TabStop = true;
+            this.ContractorRadioButton.Text = "Payroll";
+            this.ContractorRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // PermanentRadioButton
+            // 
+            this.PermanentRadioButton.AccessibleName = "Employee Type";
+            this.PermanentRadioButton.AutoSize = true;
+            this.PermanentRadioButton.Location = new System.Drawing.Point(508, 165);
+            this.PermanentRadioButton.Name = "PermanentRadioButton";
+            this.PermanentRadioButton.Size = new System.Drawing.Size(111, 24);
+            this.PermanentRadioButton.TabIndex = 8;
+            this.PermanentRadioButton.TabStop = true;
+            this.PermanentRadioButton.Text = "Consultant";
+            this.PermanentRadioButton.UseVisualStyleBackColor = true;
+            this.PermanentRadioButton.CheckedChanged += new System.EventHandler(this.PermanentRadioButton_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(130, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 30);
+            this.button1.TabIndex = 9;
+            this.button1.Text = " Add New";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(262, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PermanentRadioButton);
+            this.Controls.Add(this.ContractorRadioButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmpSal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmployeeNo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmployeeNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmpSal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton ContractorRadioButton;
+        private System.Windows.Forms.RadioButton PermanentRadioButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Week6.Test/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Week6.Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conObj = new SqlConnection(ConfigurationManager.ConnectionStrings["InfiniteCon"].ConnectionString))
+            {
+                conObj.Open();
+                SqlCommand cmdObj = new SqlCommand("InsertEmployee", conObj);
+                cmdObj.CommandType = CommandType.StoredProcedure;
+
+                // input parameters
+                cmdObj.Parameters.AddWithValue("@EmployeeId", SqlDbType.Int).Value = int.Parse(txtEmployeeNo.Text);
+                cmdObj.Parameters.AddWithValue("@EmployeeName", SqlDbType.VarChar).Value = txtEmployeeName.Text;
+                cmdObj.Parameters.AddWithValue("@EmployeeSalary", SqlDbType.Money).Value = txtEmpSal.Text;
+                cmdObj.Parameters.AddWithValue("@EmployeeType", SqlDbType.Char).Value = ContractorRadioButton.Text;
+                
+
+                // output parameter
+                SqlParameter reslt = new SqlParameter("@Result", SqlDbType.Int);
+                reslt.Direction = ParameterDirection.Output;
+                cmdObj.Parameters.Add(reslt);
+
+                // Execute procedure
+                cmdObj.ExecuteNonQuery();
+
+                int result = (int)reslt.Value;
+
+                // Validate input
+                if (txtEmployeeNo.Text.Length < 3 || txtEmployeeNo.Text.Length > 20)
+                {
+                    MessageBox.Show("Invalid EmployeeName. Length must be between 3 and 20 characters.");
+                    return;
+                }
+                if (!decimal.TryParse(txtEmpSal.Text, out decimal employeeSalary) || employeeSalary < 25000)
+                {
+                    MessageBox.Show("Invalid EmployeeSalary. Salary must be at least 25000.");
+                    return;
+                }
+                string employeeType = "";
+                if (ContractorRadioButton.Checked)
+                {
+                    employeeType = "C";
+                }
+                else if (PermanentRadioButton.Checked)
+                {
+                    employeeType = "P";
+                }
+                else
+                {
+                    MessageBox.Show("Please select either Contractor or Permanent for EmployeeType.");
+                    return;
+                }
+
+                    MessageBox.Show("Employee added successfully!");
+                }
+            }
+
+        private void PermanentRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
